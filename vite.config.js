992d671bd963d92b46dd4a97980fc04eb8087ee1
mwongeraE <evansmwongera@gmail.com>
2025-07-30 20:47:36 +0300

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vuetify from 'vite-plugin-vuetify' // Import the Vuetify plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Frontend/',
   plugins: [
     vue(),
     vuetify({
